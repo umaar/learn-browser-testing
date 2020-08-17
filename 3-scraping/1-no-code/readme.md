d97d7d5389a1823124f01d7efa788a00c5ee12ac
Umar Hansa <umar.hansa@gmail.com>
2020-08-17 19:30:13 +0100

    updates

---
+++ 3-scraping/1-no-code/readme.md
@@ -0,0 +1,5 @@
+
+## No code
+
+- https://docs.google.com/spreadsheets/d/1hep5rIaSpolaD2W09riTJ4eaCfLOX9tCUC5HdSB0wJs
+- https://simplescraper.io/
---
+++ 3-scraping/2-page-metadata/index.js
@@ -0,0 +1,26 @@
+import puppeteer from 'puppeteer';
+
+const browser = await puppeteer.launch({headless: true});
+const page = await browser.newPage();
+
+await page.goto('https://en.wikipedia.org/wiki/Space');
+
+// await page.evaluate('document.title')
+const title = await page.title(); 
+
+const image = await page.$eval('meta[property="og:image" ]', el => el.content);
+
+const text = await page.$eval('h1', el => el.textContent);
+
+const links = await page.$$eval('#mw-content-text a', els => {
+	return els.slice(0, 3).map(el => el.textContent)
+});
+
+console.log({
+	title,
+	image,
+	text,
+	links
+});
+
+await browser.close();
