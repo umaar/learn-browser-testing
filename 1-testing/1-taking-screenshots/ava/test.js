9e7026a3e82b8f60aa81ae6d97621b31738a52ba
Umar Hansa <umar.hansa@gmail.com>
2020-08-18 22:30:15 +0100

    restructure

--- 1-testing/1-taking-screenshots/ava/test.js
+++ 1-testing/1-taking-screenshots/ava/test.js
@@ -3,10 +3,10 @@ import withPage from './with-page.js';
 
 const url = 'https://example.com/';
 
-test('page title should contain "Google"', withPage, async (t, page) => {
+test('page title should be correct', withPage, async (t, page) => {
 	await page.goto(url);
 	await page.setViewport({width: 1280, height: 720});
 	await page.screenshot({path: 'ava-screenshot.png'});
 
-	t.true((await page.title()).includes('Example Domain'), 'Page title');
+	t.true((await page.title()).includes('Example Domain'), 'Page title is correct');
 });
--- 1-testing/1-taking-screenshots/puppeteer/index.js
+++ 1-testing/1-taking-screenshots/puppeteer/index.js
@@ -1,6 +1,8 @@
 import puppeteer from 'puppeteer';
 
-const browser = await puppeteer.launch();
+const browser = await puppeteer.launch({
+	headless: false
+});
 const page = await browser.newPage();
 
 await page.setViewport({ width: 1280, height: 720 });
