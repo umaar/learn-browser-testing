a9e7d68fd0bd4fbd1a1cd5fa5abf59ae6a5bc53e
Umar Hansa <umar.hansa@gmail.com>
2020-08-31 23:57:31 +0100

    Preview

--- 4-deploying/1-github-actions/jest/test.js
+++ 4-deploying/1-github-actions/jest/test.js
@@ -5,6 +5,21 @@ describe('Showcasing GitHub Actions', () => {
 
 	it('Test 1', async () => {
 		const result = await page.evaluate('1 + 1');
+		expect(result).toBe(2);
+	});
+
+	it('Test 2', async () => {
+		const result = await page.evaluate('1 + 1');
+		expect(result).toBe(2);
+	});
+
+	it('Test 3', async () => {
+		const result = await page.evaluate('1 + 1');
 		expect(result).toBe(3);
+	});
+
+	it('Test 4', async () => {
+		const result = await page.evaluate('1 + 1');
+		expect(result).toBe(2);
 	});
 });
