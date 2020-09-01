54d8e56a6aa9583b4ffc0a6a436e69739c2142da
Umar Hansa <umar.hansa@gmail.com>
2020-09-01 19:51:19 +0100

    playwright simple vids

---
+++ 4-deploying/3-videos-simple/index.js
@@ -0,0 +1,33 @@
+import {chromium} from 'playwright';
+import {mkdirSync} from 'fs';
+import path from 'path';
+import playwrightVideo from 'playwright-video';
+
+const artifactsFolder = 'test-output'
+const fullVideoPath = path.join(artifactsFolder, 'video.mp4');
+
+mkdirSync(artifactsFolder, {recursive: true});
+
+console.log('Launching Chrome');
+
+const browser = await chromium.launch({
+	slowMo: 100
+});
+
+const context = await browser.newContext();
+const page = await context.newPage();
+page.setDefaultTimeout(5000);
+
+const capture = await playwrightVideo.saveVideo(page, fullVideoPath);
+
+try {
+	await page.goto('https://umaar.com/');
+	await page.click(`a[href$="/dev-tips/"]`);
+	await page.click(`a[href$="/blog/"]`);
+	await page.click(`a[href$="/videos/"]`);
+	await page.click(`a[href$="/code/"]`);
+} finally {
+	await capture.stop();
+	await browser.close();
+	console.log('Closed browser');
+}
