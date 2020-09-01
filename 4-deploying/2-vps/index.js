a5458ae48d111a593e6a2bbccb277c3418c84162
Umar Hansa <umar.hansa@gmail.com>
2020-09-01 18:10:35 +0100

    test

--- 4-deploying/2-vps/index.js
+++ 4-deploying/2-vps/index.js
@@ -7,7 +7,7 @@ const browser = await puppeteer.launch({
 	args: ['--no-sandbox', '--disable-setuid-sandbox']
 });
 const page = await browser.newPage();
-await page.goto('https://umaar.com');
+await page.goto('https://twitter.com');
 
 console.log('Page title = ', await page.title());
 
