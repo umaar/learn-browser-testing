2ae1af63804b065b6b3ce123f5a67dfd06d95b1d
Umar Hansa <umar.hansa@gmail.com>
2020-09-01 15:36:58 +0100

    test

--- 4-deploying/2-vps/index.js
+++ 4-deploying/2-vps/index.js
@@ -7,7 +7,7 @@ const browser = await puppeteer.launch({
     args: ['--no-sandbox', '--disable-setuid-sandbox']
 });
 const page = await browser.newPage();
-await page.goto('https://amazon.com');
+await page.goto('https://duckduckgo.com');
 
 console.log('Page title = ', await page.title());
 
