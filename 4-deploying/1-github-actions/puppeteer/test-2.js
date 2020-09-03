e6769dd7f1525a3f1c161da78aa90f1a53aeb328
Umar Hansa <umar.hansa@gmail.com>
2020-09-03 12:22:20 +0100

    test

--- 4-deploying/1-github-actions/puppeteer/test-2.js
+++ 4-deploying/1-github-actions/puppeteer/test-2.js
@@ -10,7 +10,7 @@ const tests = {
 	async 'This is another test in the test-2 file'(page) {
 		await page.goto('about:blank');  
 		const result = await page.evaluate('1 + 1');
-		assert.equal(result, '2', 'The result is 2');
+		assert.equal(result, '3', 'The result is 2');
 	}
 };
 
